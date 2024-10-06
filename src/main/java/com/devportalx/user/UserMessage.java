57c63084f84fc7b1dc0b6d2f87fdc9bdb52d4323
joshoti <joshoti539@gmail.com>
2024-10-06 16:40:01 -0700

    refactor: using Messages class to ensure reusable messages

--- src/main/java/com/devportalx/user/UserMessage.java
+++ src/main/java/com/devportalx/user/UserMessage.java
@@ -1,13 +1,11 @@
 package com.devportalx.user;
 
 public class UserMessage {
+    public static final String HASHING_ALGORITHM = "SHA-256";
     public static final String USER_ALREADY_EXISTS = "User already exists";
     public static final String INVALID_PASSWORD = "User passed an invalid password";
     public static final String USER_NOT_LOGGED_IN = "User is not logged in";
     public static final String USER_CREATED_SUCCESS = "User created successfully";
     public static final String INCORRECT_USERNAME_OR_PASSWORD = "Username or password is incorrect";
-
-    public static final String INVALID_REQUEST = "Invalid request";
-    public static final String SUCCESS = "Operation successful";
     public static final String USER_NOT_FOUND = "User not found";
 }
