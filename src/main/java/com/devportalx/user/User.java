eae592c0a44d2fa97504a6dce652fd1e52c376e5
joshoti <joshoti539@gmail.com>
2024-10-06 15:59:44 -0700

    User Model is fully tested and modified

--- src/main/java/com/devportalx/user/User.java
+++ src/main/java/com/devportalx/user/User.java
@@ -1,5 +1,8 @@
 package com.devportalx.user;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -32,39 +35,82 @@ public class User {
         this.password = password;
         this.guid = UUID.randomUUID();
         this.dateJoined = LocalDate.now();
+
     }
-    
+
+    public User(String email, String password) throws NoSuchAlgorithmException {
+        this.email = email;
+        this.password = getHashedDigest(password);
+        this.guid = UUID.randomUUID();
+        this.dateJoined = LocalDate.now();
+    }
+
     @PrePersist
     protected void prePersist() {
-        if (guid == null) guid = UUID.randomUUID();
-        if (dateJoined == null) dateJoined = LocalDate.now();
+        if (guid == null)
+            guid = UUID.randomUUID();
+        if (dateJoined == null)
+            dateJoined = LocalDate.now();
     }
 
     public Long getId() {
         return id;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public byte[] getPassword() {
         return password;
     }
+
     public void setPassword(byte[] password) {
         this.password = password;
     }
+
+    /**
+     * Sets user password.
+     * 
+     * PRE: length > 0
+     * 
+     * @param password
+     * @throws NoSuchAlgorithmException
+     */
+    public void setPassword(String password) throws NoSuchAlgorithmException {
+        if (password.equals("")) {
+            return;
+        }
+        this.password = getHashedDigest(password);
+    }
+
     public UUID getGuid() {
         return guid;
     }
+
     public LocalDate getDateJoined() {
         return dateJoined;
     }
 
+    /**
+     * Used for hashing passwords so that raw values aren't saved to db
+     * 
+     * @param text
+     * @return hash of the passed in text
+     * @throws NoSuchAlgorithmException
+     */
+    public static byte[] getHashedDigest(String text) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance(UserMessage.HASHING_ALGORITHM);
+        return digest.digest(text.getBytes(StandardCharsets.UTF_8));
+    }
+
     @Override
     public String toString() {
-        return "User{" + email + "}";
+        return "User {" + email + "}";
     }
 
 }
--- src/test/java/com/devportalx/user/UserTester.java
+++ src/test/java/com/devportalx/user/UserTester.java
@@ -1,5 +1,66 @@
 package com.devportalx.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class UserTester {
+
+    private static String commonTestEmail;
+    private static String commonTestPassword;
+
+    @BeforeAll
+    public static void setUp() {
+        commonTestEmail = "james@gmail.com";
+        commonTestPassword = "password";
+    }
+
+    /**
+     * Tests the constructors, getters and setters. Ensuring that preconditions are
+     * observed.
+     * 
+     * @throws NoSuchAlgorithmException
+     */
+    @Test
+    public void testUser() throws NoSuchAlgorithmException {
+        /**
+         * Checking that constructor is called correctly, password gets hashed, and
+         * non-Null values for dateJoined and guid
+         */
+        User user = new User(commonTestEmail, commonTestPassword);
+        assertEquals(commonTestEmail, user.getEmail());
+        assertNotEquals(commonTestPassword, user.getPassword());
+        assertTrue(MessageDigest.isEqual(User.getHashedDigest(commonTestPassword), user.getPassword()));
+        assertNotNull(user.getDateJoined());
+        assertNotNull(user.getGuid());
+
+        /**
+         * Checking that email gets updated
+         */
+        user.setEmail("test@example.com");
+        assertNotEquals(commonTestEmail, user.getEmail());
+
+        /**
+         * Ensuring that invalid passwords don't get set
+         */
+        user.setPassword("");
+        assertNotEquals("", user.getPassword());
+        assertFalse(MessageDigest.isEqual(User.getHashedDigest(""), user.getPassword()));
+
+        /**
+         * Ensuring the valid passwords get hashed and set
+         */
+        user.setPassword("newPassword");
+        assertNotEquals("newPassword", user.getPassword());
+        assertTrue(MessageDigest.isEqual(User.getHashedDigest("newPassword"), user.getPassword()));
+    }
 
 }
