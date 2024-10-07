a1e166d4b961db67481c25016412766f2ab3c46d
joshoti <joshoti539@gmail.com>
2024-10-06 23:17:28 -0700

    test: added test profile to UserControllerTester

--- src/test/java/com/devportalx/user/UserControllerTester.java
+++ src/test/java/com/devportalx/user/UserControllerTester.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,6 +23,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
  * 
  * TO BE CONTINUED: when DB persists data
  */
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTester {
