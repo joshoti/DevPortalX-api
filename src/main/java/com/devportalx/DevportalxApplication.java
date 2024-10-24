437cea062b3ded3ea8934bdc3049194b943be408
joshoti <joshoti539@gmail.com>
2024-10-23 18:02:27 -0700

    Moved corsConfig to separate file

--- src/main/java/com/devportalx/DevportalxApplication.java
+++ src/main/java/com/devportalx/DevportalxApplication.java
@@ -2,24 +2,11 @@ package com.devportalx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DevportalxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevportalxApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
-			}
-		};
 	}
 }
